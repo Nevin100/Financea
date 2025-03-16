a6971832f31da77c1066aa0bab4d7f156144397a
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-03-16 23:33:57 +0530

    Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
