3bc61f574e392c16667048ef318a7308eab7888e
prasanjitagt <prasanjitacademic@gmail.com>
2025-04-14 23:31:37 +0530

    changes

---
+++ Financea/app/(payment)/payments/page.tsx
@@ -0,0 +1,11 @@
+'use client'; // Important for client-side logic
+
+
+export default function PaymentPage() {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
--- Financea/app/api/rzp/link-gen/route.ts
+++ Financea/app/api/rzp/link-gen/route.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 
 import Razorpay from "razorpay";
 import { NextRequest, NextResponse } from "next/server";
